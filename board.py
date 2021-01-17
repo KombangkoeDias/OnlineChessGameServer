88fdb9fff3148cc24ecfb1b0c7c6ced754cc1cc4
Kombangkoe Dias <kbdirifilation@gmail.com>
2021-01-17 17:17:58 +0700

    kbdapp-server1

---
+++ board.py
@@ -0,0 +1,163 @@
+import flask
+import json
+import bson.json_util as json_util
+from flask import request
+from flask_cors import CORS, cross_origin
+import datetime
+
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, verify_jwt_in_request
+)
+
+from mongoconnector import connect, addData, findOrAddNewBoardWithCode,checkCode, updateFen,LoginUser,SignupUser,addPlayer,Fen
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+app.config['JWT_SECRET_KEY'] = "Kombangkoe Dias"
+jwt = JWTManager(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+expire_access_token_time = datetime.timedelta(hours=1)
+expire_refresh_token_time = datetime.timedelta(weeks=1)
+
+localFenStorage = dict()
+
+@app.route('/code', methods=['GET'])
+@cross_origin()
+@jwt_required
+def start():
+    global localFenStorage
+    data = request.get_data()
+    code = json.loads(request.args['code'])
+    connect()
+    result = findOrAddNewBoardWithCode(code)
+    board = result['board']
+    whitePlayer = result['whitePlayer']
+    blackPlayer = result['blackPlayer']
+    return json.loads(json_util.dumps({"board": board, "white_player": whitePlayer, "black_player": blackPlayer}))
+
+
+@app.route('/getFen', methods=['POST'])
+@cross_origin()
+@jwt_required
+def getFen():
+    request.get_data()
+    data = request.get_json()
+    code = data.get('code')
+    global localFenStorage
+    fen = Fen(code)
+    '''
+    if(str(code) in localFenStorage.keys()):
+        fen = localFenStorage[code]
+    else:
+        fen = Fen(code)
+    '''
+    return json.loads(json_util.dumps({"fen": str(fen)}))
+
+@app.route('/code/check', methods=['GET'])
+@cross_origin()
+@jwt_required
+def codeCheck():
+    data = request.get_data()
+    code = json.loads(request.args['code'])
+    db = connect()
+    isCodeViable = checkCode(code)
+    return json.loads(json_util.dumps({"viable" : isCodeViable}))
+
+@app.route('/updateFen', methods=['POST'])
+@cross_origin()
+@jwt_required
+def fenUpdate():
+    request.get_data()
+    data = request.get_json()
+    code = data.get("code")
+    fen = data.get("fen")
+    db = connect()
+    try:
+        global localFenStorage
+        localFenStorage[str(code)] = fen
+        updateFen(code,fen)
+    except:
+        return json.loads(json_util.dumps({"error": True}))
+    return json.loads(json_util.dumps({"error": True}))
+
+@app.route('/login', methods=['POST'])
+@cross_origin()
+def login():
+
+    request.get_data()
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    db = connect()
+    access_token = create_access_token(username, expires_delta=expire_access_token_time)
+    refresh_token = create_refresh_token(username, expires_delta=expire_refresh_token_time)
+    try:
+        if(LoginUser(username,password)):
+            return json.loads(json_util.dumps(
+                {"error": False, "login": True, "access_token": access_token, "refresh_token": refresh_token}))
+        else:
+            return json.loads(json_util.dumps({"error": False, "login": False, "access_token": False, "refresh_token": False}))
+    except:
+        return json.loads(json_util.dumps({"error": True, "login": False, "access_token": False, "refresh_token": False}))
+
+@app.route('/signup', methods=['POST'])
+@cross_origin()
+def signup():
+    request.get_data()
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    db = connect()
+
+    access_token = create_access_token(username, expires_delta=expire_access_token_time)
+    refresh_token = create_refresh_token(username, expires_delta=expire_refresh_token_time)
+    try:
+        if(SignupUser(username,password)):
+            return json.loads(json_util.dumps(
+                {"error": False, "signup": True,"access_token": access_token, "refresh_token": refresh_token }))
+        else:
+            return json.loads(json_util.dumps(
+                {"error": False, "signup": False, "access_token": False, "refresh_token": False}))
+    except Exception as e:
+        print(e)
+        return json.loads(json_util.dumps({"error": True, "signup": False, "access_token": False, "refresh_token": False}))
+
+@app.route('/refreshToken', methods=['POST'])
+@cross_origin()
+@jwt_refresh_token_required
+def refreshToken():
+    username = get_jwt_identity()
+    access_token_new = create_access_token(username, expires_delta=expire_access_token_time)
+    refresh_token_new = create_refresh_token(username, expires_delta=expire_refresh_token_time)
+    return json.loads(json_util.dumps({"access_token": access_token_new, "refresh_token": refresh_token_new}))
+
+@app.route('/checkToken', methods=['POST'])
+@cross_origin()
+def checkLoginState():
+    try:
+        verify_jwt_in_request()
+        print("yes")
+        return json.loads(json_util.dumps({"login_state": True})), 200
+    except Exception as e:
+        print("no")
+        return json.loads(json_util.dumps({"login_state": False})), 401
+
+@app.route("/addPlayer", methods=['POST'])
+@cross_origin()
+@jwt_required
+def PlayerAdding():
+    try:
+        request.get_data()
+        data = request.get_json()
+        code = data.get("code")
+        side = data.get("side")
+        print(code)
+        print(side)
+        currCode, isPlayerAvailable = addPlayer(get_jwt_identity(),code,side)
+        return json.loads(json_util.dumps({"UserIsInCode": currCode, "available": isPlayerAvailable}))
+    except Exception as e:
+        print(e)
+connect()
---
+++ mongoconnector.py
@@ -0,0 +1,89 @@
+import pymongo
+
+db = None
+
+def connect():
+    client = pymongo.MongoClient("mongodb+srv://analytics:analytics-password@chessgame.p59zi.mongodb.net/chessgame?retryWrites=true&w=majority") 
+    global db
+    db = client.chessgame
+    return db
+
+def count(queryResult):
+    count = 0
+    for result in queryResult:
+        count += 1
+    return count
+
+def addData(code):
+    queryResult = db.board.find({"code": code})
+    if(count(queryResult) == 0):
+        print("insert new board")
+    else:
+        print("yes")
+
+def findOrAddNewBoardWithCode(code):
+    queryResult = db.board.find_one({"code": str(code)})
+    if queryResult is None :
+        print("new game")
+        startBoardfen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        db.board.insert_one({"code": str(code), "board": startBoardfen, "white": "", "black": ""})
+        return {"board": startBoardfen, "newgame": True, "whitePlayer": False, "blackPlayer": False}
+    else:
+        print("continue playing")
+        if(queryResult['white'] == ''):
+            whitePlayer = False
+        else:
+            whitePlayer = queryResult['white']
+        if(queryResult['black'] == ''):
+            blackPlayer = False
+        else:
+            blackPlayer = queryResult['black']
+        return {"board":queryResult['board'], "newgame":False, "whitePlayer": whitePlayer, "blackPlayer": blackPlayer}
+
+def addPlayer(username,code,side):
+    queryResult = db.board.find_one({"code": str(code)})
+    if (queryResult is not None):
+        if(str(side)=="white" and queryResult['white'] == ""):
+            db.board.update_one({"code": str(code)}, {"$set": {str(side): str(username)}})
+            return code, True
+        elif(str(side) == "black" and queryResult['black'] == ""):
+            db.board.update_one({"code": str(code)}, {"$set": {str(side): str(username)}})
+            return code, True
+        return code, False
+    else:
+        return None, False
+
+def checkCode(code):
+    queryResult = db.board.find_one({"code": str(code)})
+    if queryResult is None:
+        return True
+    else:
+        return False
+
+
+def updateFen(code,fen):
+    db.board.update_one({"code": str(code)}, {"$set": {"board": str(fen)}})
+
+def LoginUser(username,password):
+    queryResult = db.user.find_one({"username": username, "password": password})
+    if (queryResult is not None):
+        return True
+    return False
+
+def SignupUser(username,password):
+    queryResult = db.user.find_one({"username": username})
+    if queryResult is not None:
+        return False
+    try:
+        db.user.insert_one({"username": str(username), "password": str(password)})
+    except Exception as e:
+        print(e)
+        raise e
+    return True
+
+def Fen(code):
+    queryResult = db.board.find_one({"code": str(code)})
+    if(queryResult is not None):
+        return queryResult['board']
+    else:
+        return False
